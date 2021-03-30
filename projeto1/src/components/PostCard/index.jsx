89932096dba164d151bf77982ee989f278b6fc68
Marcelo Abrão da Silva <masilvasql@gmail.com>
2021-03-30 19:38:11 -0300

    componente

---
+++ projeto1/src/components/PostCard/index.jsx
@@ -0,0 +1,11 @@
+export const PostCard = (props)=>{
+    return(
+        <div className="post">
+        <img src={props.post.cover} alt={props.post.title} />
+        <div className='post-content'>
+          <h1>{props.post.title}</h1>
+          <p>{props.post.body}</p>
+        </div>
+      </div>
+    )
+}
